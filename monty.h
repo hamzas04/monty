56dc1d20a349f8ad82be4d8135b432823f5c4138
hamzas04 <samraoui.hamza@gmail.com>
2023-06-22 17:53:10 -0700

    0. push, pall

--- monty.h
+++ monty.h
@@ -1,23 +1,28 @@
 #ifndef MONTY_H
 #define MONTY_H
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <string.h>
-#include <ctype.h>
 
+/* Doubly linked list representation of a stack (or queue) */
 typedef struct stack_s
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
+    int n;
+    struct stack_s *prev;
+    struct stack_s *next;
 } stack_t;
 
+/* Opcode and its function */
+typedef struct instruction_s
+{
+    char *opcode;
+    void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
 /* Function prototypes */
-void push(stack_t **head, unsigned int line_number);
-void pall(stack_t **head, unsigned int line_number);
-void pint(stack_t **head, unsigned int counter);
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+/* Add more function prototypes for other opcodes */
 
 #endif /* MONTY_H */
