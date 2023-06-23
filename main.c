e7131fc264adfa09b77ccc19c09b008d08f02cc7
hamzas04 <samraoui.hamza@gmail.com>
2023-06-22 18:37:40 -0700

    4. add

--- main.c
+++ main.c
@@ -1,45 +1,48 @@
 #include "monty.h"
-bus_t bus = {NULL, NULL, NULL, 0};
+stack_t *head = NULL;
 /**
-* main - monty code interpreter
-* @argc: number of arguments
-* @argv: monty file location
-* Return: 0 on success
-*/
-int main(int argc, char *argv[])
+ * main - entry point
+ * @argc: number of args
+ * @argv: filenames
+ * Return: success always
+ */
+int main(int argc, char **argv)
 {
-	char *content;
-	FILE *file;
-	size_t size = 0;
-	ssize_t read_line = 1;
-	stack_t *stack = NULL;
-	unsigned int counter = 0;
+	char buffer[MAX];
+	data_t *data;
 
 	if (argc != 2)
 	{
-		fprintf(stderr, "USAGE: monty file\n");
+		dprintf(STDERR_FILENO, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	file = fopen(argv[1], "r");
-	bus.file = file;
-	if (!file)
+
+	data = malloc(sizeof(data_t));
+
+	if (!data)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-	while (read_line > 0)
+
+	data->line = 1;
+	data->fd = fopen(argv[1], "r");
+
+	if (data->fd == NULL)
 	{
-		content = NULL;
-		read_line = getline(&content, &size, file);
-		bus.content = content;
-		counter++;
-		if (read_line > 0)
-		{
-			execute(content, &stack, counter, file);
-		}
-		free(content);
+		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
 	}
-	free_stack(stack);
-	fclose(file);
-return (0);
+
+	while (fgets(buffer, MAX, data->fd) != NULL)
+	{
+		parse(buffer, data, &head);
+		data->line++;
+	}
+
+	fclose(data->fd);
+	if (data)
+		free(data);
+	free_dlistint(head);
+	return (EXIT_SUCCESS);
 }
